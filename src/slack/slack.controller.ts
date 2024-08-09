db6410c0a4e5616e14f0d61ad6dc923894db55ea
Ritchey <oluseyer@gmail.com>
2024-08-09 17:59:43 +0100

    refactor: update auto reminder schedule

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -1,22 +1,28 @@
 import { Controller } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { SlackService } from './slack.service';
 
 // Todo: move to config
-const MONDAY_TO_FRIDAY_AT_11AM = '0 0 11 * * 1-5';
-const MONDAY_TO_FRIDAY_AT_4_30PM = '0 30 16 * * 1-5';
+const MONDAY_TO_FRIDAY_AT_09_30AM = CronExpression.MONDAY_TO_FRIDAY_AT_09_30AM;
+const MONDAY_TO_FRIDAY_AT_1PM = CronExpression.MONDAY_TO_FRIDAY_AT_1PM;
+const MONDAY_TO_FRIDAY_AT_4PM = CronExpression.MONDAY_TO_FRIDAY_AT_4PM;
 
 @Controller('slack')
 export class SlackController {
   constructor(private readonly slackService: SlackService) {}
 
-  @Cron(MONDAY_TO_FRIDAY_AT_11AM)
+  @Cron(MONDAY_TO_FRIDAY_AT_09_30AM)
   handleMorningReminderCron() {
     return this.slackService.triggerReviewReminders();
   }
 
-  @Cron(MONDAY_TO_FRIDAY_AT_4_30PM)
+  @Cron(MONDAY_TO_FRIDAY_AT_1PM)
   handleAfternoonReminderCron() {
+    return this.slackService.triggerReviewReminders();
+  }
+
+  @Cron(MONDAY_TO_FRIDAY_AT_4PM)
+  handleEveningReminderCron() {
     return this.slackService.triggerReviewReminders();
   }
 
