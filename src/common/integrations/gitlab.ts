013932c00a0ecde4dedac5b9382415500c35c058
Ritchey <oluseyer@gmail.com>
2024-09-24 15:13:59 +0100

    add gitlab sdk setup

---
+++ src/common/integrations/gitlab.ts
@@ -0,0 +1,12 @@
+import { Gitlab } from '@gitbeaker/rest';
+
+if (!process.env.GITLAB_TOKEN) {
+  throw new Error('Gitlab access token is missing');
+}
+
+const api = new Gitlab({
+  host: process.env.GITLAB_HOST || 'https://gitlab.com',
+  token: process.env.GITLAB_TOKEN || '',
+});
+
+export default api;
