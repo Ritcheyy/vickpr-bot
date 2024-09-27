fae7975ec18c40efc7423aa862ea6f55e99155c2
Ritchey <oluseyer@gmail.com>
2024-09-27 21:24:15 +0100

    add pull request handler

--- src/pull-requests/integrations/gitlab.service.ts
+++ src/pull-requests/integrations/gitlab.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Gitlab } from '@gitbeaker/core';
+import { GitlabEventActions, GitlabEvents } from '@/common/constants';
 import GitlabClient from '../../common/integrations/gitlab';
 
 @Injectable()
@@ -21,7 +22,19 @@ export class GitlabService {
   }
 
   async handlePullRequestEvent(payload) {
+    const { object_kind, object_attributes } = payload;
+
     try {
+      // Check event type
+      if (object_kind === GitlabEvents.NOTE) {
+      } else {
+        // Merge request type
+        if (object_attributes.action === GitlabEventActions.OPEN) {
+          // create a new pull request
+        } else {
+          // Update existing one
+        }
+      }
       // Check action type
     } catch (errors) {
       console.log(errors);
