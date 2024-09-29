de624e8fe33b12d6931901db64d025a26931550c
Ritchey <oluseyer@gmail.com>
2024-09-29 12:59:04 +0100

    add pr-template file

---
+++ src/common/templates/pr-default.md
@@ -0,0 +1,27 @@
+### Pull Request Type
+
+- Type of pull request (feature, bugfix, hotfix, etc.)
+
+### Changes
+
+- Description of changes
+
+### How To Test
+
+- Outline the steps the reviewer would take in testing the PR
+
+### Functionality Reviewer
+
+- Tag the reviewers of this PR (this will be used to link their Slack profiles)
+
+### Jira Link
+
+- Link to Jira task
+
+### Screenshots / Postman Link
+
+- (Please add necessary screenshots or a postman documentation link)
+
+### Priority
+
+- Medium
--- src/pull-requests/integrations/gitlab.service.ts
+++ src/pull-requests/integrations/gitlab.service.ts
@@ -13,26 +13,34 @@ export class GitlabService {
 
   async fetchPR(projectId: string | number, prId: number) {
     try {
-      const projects = await this.apiClient.MergeRequests.show(projectId, prId);
-      console.log(projects);
-      return projects;
+      return await this.apiClient.MergeRequests.show(projectId, prId);
     } catch (errors) {
       throw errors;
     }
   }
 
-  async handlePullRequestEvent(payload) {
-    const { object_kind, object_attributes } = payload;
+  async handlePullRequestEvent(payload: Record<any, any>) {
+    const { project, object_kind, object_attributes } = payload;
 
     try {
       // Check event type
       if (object_kind === GitlabEvents.NOTE) {
+        // Handle pull request comment
       } else {
         // Merge request type
         if (object_attributes.action === GitlabEventActions.OPEN) {
+          // get the pull request details
+          // const newPullRequest = await this.fetchPR(project.id, object_attributes.iid);
+          // console.log(newPullRequest);
           // create a new pull request
         } else {
           // Update existing one
+          const existingPullRequest = await this.fetchPR(project.id, object_attributes.iid);
+          const pullRequest = {
+            title: existingPullRequest.title,
+            link: existingPullRequest.web_url,
+          };
+          console.log(pullRequest);
         }
       }
       // Check action type
