b5fa57dff5681d480758b4cbeb12616c6505ef02
Ritchey <oluseyer@gmail.com>
2025-11-05 22:28:01 +0100

    chore: used import

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { SlackService } from './slack.service';
 import { isWeeklyReportEnabled } from '@/common/config';
